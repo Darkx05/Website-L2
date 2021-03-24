afa11dc180589730ea57d2e6669444c5860908f9
Darkx05 <avishai.lumanlan@gmail.com>
2021-03-25 08:56:19 +1300

    25/3/2021
    added css and php and fonts

---
+++ 91893 FINAL/tourismindex.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<html>
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+ <meta name="description" content="Tourism in New Zealand">
+ <meta name="keywords" content="index, places to visit, acommodation, transportation, about nz, contact us">
+ <meta name="author" content="Avishai Lumanlan">
+ 
+ <title>Tourism in NZ</title>
+ 
+ <!-- link to css goes here -->
+ <link href="css/poetry_style.css" rel="stylesheet">
+ <!--- link to google fonts go here --->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@500&family=Lexend:wght@500&family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+ </head>
+    
+    <body>
+    <h1> HELLO WORLD!</h1>
+        <p>Paragraph text sample</p>
+        
+        
+    </body>
+
+
+</html>
--- README.md
+++
@@ -1,2 +0,0 @@
-# Website-L2
-91893 assessment. Work will also be uploaded to projectspace.
